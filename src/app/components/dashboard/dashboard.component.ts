8115a2c3a3a330984d3be10f5374eb7e751a9564
k-chiranjeevi <chiru8004@gmail.com>
2024-06-06 14:07:08 +0530

    added user details

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -8,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
   constructor() { }
-
-  ngOnInit(): void {
+  userdetails: any;
+  ngOnInit() {
+    this.userdetails = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user') as string) : null
   }
 
 }
